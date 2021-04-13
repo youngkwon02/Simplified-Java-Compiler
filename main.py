7caa8e1366dc552126782a8b3964fc8f0849023a
Youngkwon Kim <jakealden97@gmail.com>
2021-04-13 17:34:32 +0900

    fix: arithmetic&signedInt bug for '-'

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from dfa import DFA, is_comparison, is_identifier
+import string
+DIGIT = string.digits
 
 def read_input_file():
-    file = open("./test_code/test2.java", mode='r', encoding='utf-8')
+    file = open("./test_code/test.java", mode='r', encoding='utf-8')
     return file
 
 def init_success_dfa():
-    return ["KEYWORD", "BOOLEAN", "ASSIGNMENT_OPERATORS", "COMPARISON_OPERATORS", "SIGNED_INTEGER", "ARITHMETIC_OPERATORS", "TERMINATING_SYMBOL", "LEFT_PAREN", "RIGHT_PAREN",
+    return ["KEYWORD", "BOOLEAN", "ASSIGNMENT_OPERATOR", "COMPARISON_OPERATOR", "SIGNED_INTEGER", "ARITHMETIC_OPERATOR", "TERMINATING_SYMBOL", "LEFT_PAREN", "RIGHT_PAREN",
     "LEFT_BRACE", "RIGHT_BRACE", "LEFT_BRANKET", "RIGHT_BRANKET", "COMMA", "WHITESPACE", "VTYPE", "IDENTIFIER", "SINGLE_CHARACTER", "LITERAL_STRING"]
 
 
 def init_each_dfa(DFA_list):
     for dfa in DFA_list:
         dfa.initialize()
+
+def check_before_input(index, content):
+    while(index > 0):
+        if(content[index] not in string.whitespace):
+            return content[index]
+        index -= 1
+
+    return("")
 
 def main():
     DFA_vtype = DFA("VTYPE")
@@ -21,9 +31,9 @@ def main():
     DFA_boolean = DFA("BOOLEAN")
     DFA_literalString = DFA("LITERAL_STRING")
     DFA_keyword = DFA("KEYWORD")
-    DFA_arithmeticOperators = DFA("ARITHMETIC_OPERATORS")
-    DFA_assignmentOperators = DFA("ASSIGNMENT_OPERATORS")
-    DFA_comparisonOperators = DFA("COMPARISON_OPERATORS")
+    DFA_arithmeticOperators = DFA("ARITHMETIC_OPERATOR")
+    DFA_assignmentOperators = DFA("ASSIGNMENT_OPERATOR")
+    DFA_comparisonOperators = DFA("COMPARISON_OPERATOR")
     DFA_terminatingSymbol = DFA("TERMINATING_SYMBOL")
     DFA_leftParen = DFA("LEFT_PAREN")
     DFA_rightParen = DFA("RIGHT_PAREN")
@@ -37,7 +47,7 @@ def main():
     DFA_terminatingSymbol, DFA_leftParen, DFA_rightParen, DFA_leftBrace, DFA_rightBrace, DFA_leftBranket, DFA_rightBranket, DFA_comma, DFA_whitespace,
     DFA_vtype, DFA_identifier, DFA_singleCharacter, DFA_literalString
     ] # 모든 DFA List, Ordered by priority
-    success_DFA = ["KEYWORD", "BOOLEAN", "ASSIGNMENT_OPERATORS", "COMPARISON_OPERATORS", "SIGNED_INTEGER", "ARITHMETIC_OPERATORS", "TERMINATING_SYMBOL", "LEFT_PAREN", "RIGHT_PAREN",
+    success_DFA = ["KEYWORD", "BOOLEAN", "ASSIGNMENT_OPERATOR", "COMPARISON_OPERATOR", "SIGNED_INTEGER", "ARITHMETIC_OPERATOR", "TERMINATING_SYMBOL", "LEFT_PAREN", "RIGHT_PAREN",
     "LEFT_BRACE", "RIGHT_BRACE", "LEFT_BRANKET", "RIGHT_BRANKET", "COMMA", "WHITESPACE", "VTYPE", "IDENTIFIER", "SINGLE_CHARACTER", "LITERAL_STRING"]
 
     f = read_input_file()
@@ -47,7 +57,6 @@ def main():
     keyword_flag = False
     while(True):
         input_char = file_content[index]
-        # print(index, "/", input_char)
         for dfa in DFA_list:
             if dfa.get_name() in success_DFA:
                 result = dfa.run(input_char)
@@ -66,20 +75,25 @@ def main():
                                 else:
                                     success_DFA.remove("KEYWORD")
                                     continue
-                            elif success == "ASSIGNMENT_OPERATORS":
+                            elif success == "ASSIGNMENT_OPERATOR":
                                 if not is_comparison(DFA_comparisonOperators.current_state, file_content[index]):
-                                    text = "<ASSIGNMENT_OPERATORS> " + result[1] + "\n"
+                                    text = "<ASSIGNMENT_OPERATOR> " + result[1] + "\n"
                                     f_output.write(text)
                                     index -= 1
                                     break
                                 else:
-                                    text = "<COMPARISON_OPERATORS> " + "==" + "\n"
+                                    text = "<COMPARISON_OPERATOR> " + "==" + "\n"
                                     f_output.write(text)
                                     break
                             else:
                                 if success != "WHITESPACE":
-                                    text = "<" + success + "> " + result[1] + "\n"
-                                    f_output.write(text)
+                                    if result[1][0] == "-" and not index-1 - len(result[1])<0 and check_before_input(index-1 - len(result[1]), file_content) in DIGIT:
+                                        text = "<ARITHMETIC_OPERATOR> " + "-" + "\n"
+                                        index -= (len(result[1]) - 1)
+                                        f_output.write(text)
+                                    else:
+                                        text = "<" + success + "> " + result[1] + "\n"
+                                        f_output.write(text)
                                 index -= 1
                                 break
                         if (dfa.get_name() != "KEYWORD" or keyword_flag == True):
@@ -92,8 +106,14 @@ def main():
                         if result[0] == "going":
                             for success in success_DFA:
                                 if success != "WHITESPACE":
-                                    text = "<" + success + "> " + dfa.get_record() + "\n"
-                                    f_output.write(text)
+                                    # 아래 line에서 Index는 마지막 글자라서 보통 case보다 이미 1이 작음, index에서 1을 따로 빼지 않고 check_before_input
+                                    if dfa.get_record()[0] == "-" and check_before_input(index - len(dfa.get_record()), file_content) in DIGIT:
+                                        text = "<ARITHMETIC_OPERATOR> " + "-" + "\n"
+                                        index -= (len(dfa.get_record()) - 1)
+                                        f_output.write(text)
+                                    else:
+                                        text = "<" + success + "> " + dfa.get_record() + "\n"
+                                        f_output.write(text)
                                 init_each_dfa(DFA_list)
                                 success_DFA = init_success_dfa()
                                 break
